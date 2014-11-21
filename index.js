78839e6a42d0f1f13192367b0cd7b7cdc0251fb6
Ben Van Treese <vantreeseba@gmail.com>
2014-11-21 15:51:25 -0600

    removed process exit, using base runner

--- index.js
+++ index.js
@@ -1,76 +1,91 @@
-module.exports = interactiveHtmlReporter;
+exorts = module.exports = interactiveHtmlReporter;
 
 var fs = require('fs');
 var jquery = fs.readFileSync(__dirname + '/lib/jquery.js');
 var script = fs.readFileSync(__dirname + '/lib/script.js');
 
+var Base, log;
+
+if (typeof window === 'undefined') {
+    //running in Node
+    Base = require('mocha').reporters.Base;
+    log = console.log;
+} else {
+    // running in mocha-phantomjs
+    Base = require('./base');
+    log = function(msg) {
+        process.stdout.write(msg + "\n");
+    };
+}
+
 function interactiveHtmlReporter(runner) {
-	var passes = 0;
-	var failures = 0;
-	var indentLevel = 0;
-	var outputFile = '<html>';
+    Base.call(this, runner);
+    var stats = this.stats;
 
-	outputFile += '<head><script type="text/javascript">' +
-		jquery +
-		'</script>';
+    var passes = 0;
+    var failures = 0;
+    var indentLevel = 0;
+    var outputFile = '<html>';
 
-	outputFile += '<head><script type="text/javascript">' +
-		script +
-		'</script>';
+    outputFile += '<head><script type="text/javascript">' +
+        jquery +
+        '</script>';
 
-	outputFile += '<style>' +
-		'.pass { color: green; }' +
-		'.fail { color: red; }' +
-		'.suite.open:before {content: ">"}' +
-		'.suite.closed:before {content: "^"}' +
-		'</style></head><body>';
+    outputFile += '<head><script type="text/javascript">' +
+        script +
+        '</script>';
 
-	function insertSuiteStart(suite) {
-		indentLevel++;
-		if (indentLevel > 1) {
-			outputFile += '<div class="suite open" ' +
-				'style="padding-left: ' + indentLevel * 10 + 'px">' +
-				suite.title;
-		}
-	}
+    outputFile += '<style>' +
+        '.pass { color: green; }' +
+        '.fail { color: red; }' +
+        '.suite.open:before {content: ">"}' +
+        '.suite.closed:before {content: "^"}' +
+        '</style></head><body>';
 
-	function insertSuiteEnd() {
-		indentLevel--;
-		outputFile += '</div>';
-	}
+    function insertSuiteStart(suite) {
+        indentLevel++;
+        if (indentLevel > 1) {
+            outputFile += '<div class="suite open" ' +
+                'style="padding-left: ' + indentLevel * 10 + 'px">' +
+                suite.title;
+        }
+    }
 
-	function insertPassingTest(test) {
-		passes++;
-		outputFile += '<div class="pass">' +
-			test.title +
-			'</div>';
-	}
+    function insertSuiteEnd() {
+        indentLevel--;
+        outputFile += '</div>';
+    }
 
-	function insertFailingTest(test, err) {
-		failures++;
-		outputFile += '<div class="fail">' +
-			test.title +
-			'<br />' +
-			err +
-			'</div>';
-	}
+    function insertPassingTest(test) {
+        passes++;
+        outputFile += '<div class="pass">' +
+            test.title +
+            '</div>';
+    }
 
-	runner.on('suite', insertSuiteStart);
-	runner.on('suite end', insertSuiteEnd);
-	runner.on('pass', insertPassingTest);
-	runner.on('fail', insertFailingTest);
+    function insertFailingTest(test, err) {
+        failures++;
+        outputFile += '<div class="fail">' +
+            test.title +
+            '<br />' +
+            err +
+            '</div>';
+    }
 
-	runner.on('end', function() {
-		outputFile += "</body></html>";
-		if (!fs.existsSync('./artifacts')) {
-			fs.mkdirSync('./artifacts');
-		}
-		if (!fs.existsSync('./artifacts/mocha')) {
-			fs.mkdirSync('./artifacts/mocha');
-		}
+    runner.on('suite', insertSuiteStart);
+    runner.on('suite end', insertSuiteEnd);
+    runner.on('pass', insertPassingTest);
+    runner.on('fail', insertFailingTest);
 
-		fs.writeFileSync('./artifacts/mocha/index.html', outputFile);
+    runner.on('end', function() {
+        outputFile += "</body></html>";
+        if (!fs.existsSync('./artifacts')) {
+            fs.mkdirSync('./artifacts');
+        }
+        if (!fs.existsSync('./artifacts/mocha')) {
+            fs.mkdirSync('./artifacts/mocha');
+        }
 
-		process.exit(failures);
-	});
+        fs.writeFileSync('./artifacts/mocha/index.html', outputFile);
+    });
 }
